4d68c02add06df8f92d06ef55f71c781d11a141d
MariaGolomb <marialukashuk.ml@gmail.com>
2019-07-19 16:12:40 +0300

    part 2, props

--- src/App.js
+++ src/App.js
@@ -6,16 +6,27 @@ import './components/SearchBar/SearchBar.js';
 import SeachBar from './components/SearchBar/SearchBar.js';
 import BusinessList from './components/BusinessList/businessList.js';
 
+const business={
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses=[business, business, business, business, business, business];
 
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SeachBar />
-      <BusinessList />
-
+      <BusinessList businesses={businesses} />
     </div>
-
   );
 }
 
